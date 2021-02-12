c738674fb08f53fba7df4d2ef14588b54d44474f
ryanxuan930 <anakinchung930@gmail.com>
2021-02-12 17:42:04 +0800

    Update signal.php

--- server/signal.php
+++ server/signal.php
@@ -1,3 +1,8 @@
 <?php
-echo $_POST["timestamp"]."-".$_POST["status"];
+include_once("C:\wamp64\www\hsnua3\database.php");
+$timestamp = $_POST["timestamp"];
+$status = $_POST["status"];
+$conn->select_db("timing");
+$conn->query("UPDATE status SET timestamp={$timestamp}, status={$status}");
+$conn->close();
 ?>
