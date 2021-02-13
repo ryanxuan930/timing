45d7493ba2f349650d2329f67831a07655908252
ryanxuan930 <anakinchung930@gmail.com>
2021-02-13 18:38:14 +0800

    Update result.php

--- server/result.php
+++ server/result.php
@@ -1,7 +1,7 @@
 <?php
 include_once("C:\wamp64\www\hsnua3\database.php");
-$result = $_POST["result"];
 $rank = $_POST["rank"];
+$result = $_POST["result"];
 $stmt = $conn->prepare("INSERT INTO timing.result (rank,result) VALUES (?,?)");
 $stmt->bind_param("is",$rank,$result);
 $stmt->execute();
