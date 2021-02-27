c6d25c61aa05c400c32f247160a8d863f05471ad
ryanxuan930 <anakinchung930@gmail.com>
2021-02-28 02:17:11 +0800

    Update submit_result.php

--- server/submit_result.php
+++ server/submit_result.php
@@ -3,4 +3,9 @@ include_once("C:\wamp64\www\hsnua3\dbclass.php");
 $db = new database("timing");
 $db->table("result");
 $db->update("submit=1","submit=0");
+if($db->error()){
+    echo $db->error();
+}else{
+    echo "已送出成績";
+}
 ?>
