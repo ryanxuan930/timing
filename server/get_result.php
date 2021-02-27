b40e40a8dfaa27fcd4b7c0813856dab3a26f7112
ryanxuan930 <anakinchung930@gmail.com>
2021-02-27 19:46:53 +0800

    Update get_result.php

--- server/get_result.php
+++ server/get_result.php
@@ -2,9 +2,14 @@
 include_once("C:\wamp64\www\hsnua3\dbclass.php");
 $db = new database("timing");
 $db->table("result");
-$result = $db->select("rank");
+$result = $db->select_order("rank,result,bib","rank DESC");
 $array = array();
-$array["ranking"] = $result->num_rows;
+$i=0;
+while($row = $result->fetch_row()){
+    $array[$i][0]==$array[1];
+    $array[$i][1]==$array[2];
+    $i++;
+}
 $obj = json_encode($array);
 echo $obj;
 ?>
