a40b9c1a17100bad7be25a7657efeb1a54415170
ryanxuan930 <anakinchung930@gmail.com>
2021-02-28 02:18:41 +0800

    Update reset.php

--- server/reset.php
+++ server/reset.php
@@ -1,5 +1,9 @@
 <?php
 include_once("C:\wamp64\www\hsnua3\database.php");
+include_once("C:\wamp64\www\hsnua3\dbclass.php");
+$db = new database("timing");
+$db->table("status");
+$db->update("submit=0","submit=1");
 $conn->query("TRUNCATE TABLE timing.result");
 if($conn->error){
     echo $conn->error;
