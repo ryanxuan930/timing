7c6fa7dbf680f2b44537325cae615f0824f1f392
ryanxuan930 <anakinchung930@gmail.com>
2021-02-27 12:03:12 +0800

    Update get_result.php

--- server/get_result.php
+++ server/get_result.php
@@ -1,4 +1,10 @@
 <?php
 include_once("C:\wamp64\www\hsnua3\dbclass.php");
 $db = new database("timing");
+$db->table("result");
+$result = $db->select("rank");
+$array = array();
+$array["ranking"] = $result->num_rows;
+$obj = json_encode($array);
+echo $obj;
 ?>
