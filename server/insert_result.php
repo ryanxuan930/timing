f99a1b5b94240694765337470d9f7e3b59ece1b2
ryanxuan930 <anakinchung930@gmail.com>
2021-02-27 23:20:39 +0800

    Update insert_result.php

--- server/insert_result.php
+++ server/insert_result.php
@@ -3,7 +3,7 @@ include_once("C:\wamp64\www\hsnua3\database.php");
 $rank = $_POST["ranking"];
 $bib = $_POST["bib"];
 $stmt = $conn->prepare("UPDATE timing.result SET bib=? WHERE rank=?");
-$stmt->bind_param("si",$result,$rank);
+$stmt->bind_param("si",$bib,$rank);
 $stmt->execute();
 if($conn->error){
     echo "儲存失敗：";
