5e948c5c4d9d9d0827a1d83917a73292a907be05
ryanxuan930 <anakinchung930@gmail.com>
2021-02-27 19:51:39 +0800

    Update get_result.php

--- server/get_result.php
+++ server/get_result.php
@@ -6,8 +6,12 @@ $result = $db->select_order("rank,result,bib","rank DESC");
 $array = array();
 $i=0;
 while($row = $result->fetch_row()){
-    $array[$i][0]==$array[1];
-    $array[$i][1]==$array[2];
+    $array[$i][0]==$row[1];
+    if($row[1]){
+        $array[$i][1]==$row[2];
+    }else{
+        $array[$i][1]=="";
+    }
     $i++;
 }
 $obj = json_encode($array);
